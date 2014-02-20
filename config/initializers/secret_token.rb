17e990d6ac01f04bc2b7a6d52d8b20cd40e4618c
Kevin Falank <kevinf@carolina.rr.com>
2014-02-20 12:53:47 -0600

    rails skeleton with prostgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GentlemansBet::Application.config.secret_key_base = '8db04cff8bd38082439e5ca7934e3057a3855cbfd8624f9d4152e6c18756888739b2461597a19fc83dbc4059b3782d9e26db53845177d996bc8881de8dbb227b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GentlemansBet::Application.config.session_store :cookie_store, key: '_gentlemans_bet_session'
