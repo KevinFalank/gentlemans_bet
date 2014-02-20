1caca359fc750b7808c4e165a1b43e0a3ed994fa
Kevin Falank <kevinf@carolina.rr.com>
2014-02-20 16:22:10 -0600

    travis test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#addition" do
+  	it "sample test" do
+  		user = User.create(username: "kevin")
+
+  		expect(user.add_2).to eq(4)
+
+  	end
+  end
+  
 end
