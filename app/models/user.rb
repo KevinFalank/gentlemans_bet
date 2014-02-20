17ca2ff09bc3968fdb2fb825f73788191c7f8ab8
Kevin Falank <kevinf@carolina.rr.com>
2014-02-20 14:49:54 -0600

    Added associations and corrected database connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :challenges_created, class_name: "Challenge", foreign_key: "challenger_id"
+	has_many :challenges_received, class_name: "Challenge", foreign_key: "challengee_id"
+	has_many :challenges_won, class_name: "Challenge", foreign_key: "winner_id"
+
 end
