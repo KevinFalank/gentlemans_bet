fea179c80aa2727319bdafb8bb105ed043790d20
Apprentice <apprentice@dbc-chicago-004.local>
2014-02-21 13:40:08 -0600

    complete twitter oauth sign in; add challenges routing, controller, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 GentlemansBet::Application.routes.draw do
+  get 'sign_in' => 'twitter#sign_in'
+  get 'auth' => 'twitter#auth'
+
+  resources :challenges
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +44,7 @@ GentlemansBet::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221170017) do
+ActiveRecord::Schema.define(version: 20140221171227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140221170017) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "access_secret"
   end
 
 end
