f11f1c795a7e6324a1d0a64d5d396d10daca2b31
Laura <lleecrowe@gmail.com>
2014-02-21 22:34:27 -0600

    added test to reminder.remind

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -12,6 +12,7 @@ class Reminder
   end
 
   def self.remind #this is the method we will call in our rake task
+    puts "You have been reminded" #temporary test
     #include one last method that will send the actual tweet 
     #containing the url from the challenge (search for challenge
     #by id...key in hash is the challenge id)
