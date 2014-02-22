4b07e4a3c90c2da5cf573f6e6e7b1116160961ea
Laura <lleecrowe@gmail.com>
2014-02-21 22:16:44 -0600

    playing with rspec

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
 describe Reminder do
-  # let(:challenge1){ Challenge.new(challenger_id: 1,
-  #                                 challengee_id:  2,
-  #                                 end_date:   Date.new(2014,2,22)) }
-  # let(:challenge2){ Challenge.new(challenger_id: 3,
-  #                                 challengee_id:  4,
-  #                                 end_date:   Date.new(2014,2,27)) }
-  # let(:challenge3){ Challenge.new(challenger_id: 5,
-  #                                 challengee_id:  6,
-  #                                 end_date:   Date.new(2014,2,22)) }
 
   it "should find challenges that expire tomorrow" do
     challenge1 = Challenge.create(end_date: Date.new(2014,2,22))
@@ -21,10 +12,10 @@ describe Reminder do
 
 
   it "should select the users that are participating in the challenges that expire tomorrow" do
-    challenge1 = Challenge.create(challenger_id: 1, 
+    challenge4 = Challenge.create(challenger_id: 1, 
                                   challengee_id: 2, 
                                   end_date: Date.new(2014,2,22))
-    challenge2 = Challenge.create(challenger_id: 3, 
+    challenge5 = Challenge.create(challenger_id: 3, 
                                   challengee_id: 4, 
                                   end_date: Date.new(2014,2,27))
     expect(Reminder.send_to).to eq({4=>[1, 2]})
