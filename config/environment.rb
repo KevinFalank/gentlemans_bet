c0b3fc93f56237a069570e4342ca49853cf3fc05
Kevin Falank <kevinf@carolina.rr.com>
2014-02-22 10:47:16 -0600

    Implemented Dotenv to handle environment variables and removed twitter.yaml

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,14 @@ require File.expand_path('../application', __FILE__)
 GentlemansBet::Application.initialize!
 
 # Set up the twitter env variables
-APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+# APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+# env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
 
-env_config.each do |key, value|
-  ENV[key] = value
-end
+# env_config.each do |key, value|
+#   ENV[key] = value
+# end
 
-
+# environemtn variables are now set via Dotenv
 Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_KEY']
   config.consumer_secret = ENV['TWITTER_SECRET']
