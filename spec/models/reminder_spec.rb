0b0b30e31b88b04a41fa7b1f27dcdcb6a46787b2
Laura <lleecrowe@gmail.com>
2014-02-21 18:56:34 -0600

    debugged and have rspec test for send_to in reminders model

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe Reminder do
 
   it "should find challenges that expire tomorrow" do
-    challenge1 = Challenge.new(end_date: Date.new(2014,2,22))
-    challenge2 = Challenge.new(end_date: Date.new(2014,2,27))
-    challenge3 = Challenge.new(end_date: Date.new(2014,2,22))
+    challenge1 = Challenge.create(end_date: Date.new(2014,2,22))
+    challenge2 = Challenge.create(end_date: Date.new(2014,2,27))
+    challenge3 = Challenge.create(end_date: Date.new(2014,2,22))
 
     expect(Reminder.expiring_bets).to eq([challenge1, challenge3])
   end
 
 
   it "should select the users that are participating in the challenges that expire tomorrow" do
-    challenge1 = Challenge.new(challenger_id: 2, 
-                                  challengee_id: 1, 
+    challenge1 = Challenge.create(challenger_id: 1, 
+                                  challengee_id: 2, 
                                   end_date: Date.new(2014,2,22))
-    challenge2 = Challenge.new(challenger_id: 3, 
+    challenge2 = Challenge.create(challenger_id: 3, 
                                   challengee_id: 4, 
                                   end_date: Date.new(2014,2,27))
     expect(Reminder.send_to).to eq([1, 2])
