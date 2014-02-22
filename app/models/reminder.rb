7ef19d6b5dc9caacd1e9d447dfa34814306bd470
Laura <lleecrowe@gmail.com>
2014-02-21 18:06:21 -0600

    adding rspec to test which users we are reminding

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,7 +1,16 @@
 class Reminder
 
-  def self.send_to
+  def self.expiring_bets
     Challenge.where(end_date: (Date.today + 1))
+  end
+
+  def self.send_to
+  	recipients = []
+  	Reminder.expiring_bets.each do |bet|
+  		recipients << bet.challenger_id
+      recipients << bet.challengee_id
+  	end
+    recipients
   end
 
 end
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -2,13 +2,23 @@ require 'spec_helper'
 
 describe Reminder do
 
-	it "should check for challenges that expire tomorrow" do
-    challenge1 = Challenge.create(end_date: Date.new(2014,2,22))
-    challenge2 = Challenge.create(end_date: Date.new(2014,2,22))
-    challenge3 = Challenge.create(end_date: Date.new(2014,2,27))
+  it "should find challenges that expire tomorrow" do
+    challenge1 = Challenge.new(end_date: Date.new(2014,2,22))
+    challenge2 = Challenge.new(end_date: Date.new(2014,2,27))
+    challenge3 = Challenge.new(end_date: Date.new(2014,2,22))
 
-    expect(Reminder.send_to).to eq([challenge1, challenge2])
+    expect(Reminder.expiring_bets).to eq([challenge1, challenge3])
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should select the users that are participating in the challenges that expire tomorrow" do
+    challenge1 = Challenge.new(challenger_id: 2, 
+                                  challengee_id: 1, 
+                                  end_date: Date.new(2014,2,22))
+    challenge2 = Challenge.new(challenger_id: 3, 
+                                  challengee_id: 4, 
+                                  end_date: Date.new(2014,2,27))
+    expect(Reminder.send_to).to eq([1, 2])
+  end
+
 end
