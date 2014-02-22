7046a2ab4994991fda6ff99dfce7f45d6f51fcbb
Laura <lleecrowe@gmail.com>
2014-02-21 18:52:25 -0600

    fixing twitter config

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ env_config.each do |key, value|
 end
 
 
-Twitter.configure do |config|
+Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_KEY']
   config.consumer_secret = ENV['TWITTER_SECRET']
 end
